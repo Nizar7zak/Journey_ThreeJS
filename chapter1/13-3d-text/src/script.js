d4146871153d5a213591ba5419d3168f4801f412
nizar7zak <nizar7zakout@gmail.com>
2023-04-23 20:11:43 +0200

    16-shadow

--- chapter1/13-3d-text/src/script.js
+++ chapter1/13-3d-text/src/script.js
@@ -71,10 +71,10 @@ fontLoader.load(
 
             donut.rotation.x = Math.random() * Math.PI
             donut.rotation.y = Math.random() * Math.PI
-            scene.add(donut)
-
+            
             const scale = Math.random()
             donut.scale.set(scale, scale, scale)
+            scene.add(donut)
         }
         
     }
