72a4c14badd59c6e35ac8691f9dd775621018fac
nizar7zak <nizar7zakout@gmail.com>
2023-05-04 15:02:35 +0200

    Structure

---
+++ chapter2/26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -0,0 +1,81 @@
+import * as THREE from 'three'
+
+import Sizes from "./Utils/Sizes"
+import Time from "./Utils/Time"
+import Resources from './Utils/Resources'
+import Camera from './Camera'
+import Renderer from './Renderer'
+import sources from './sources'
+import World from './World/World'
+import Debug from './Utils/Debug'
+
+let instance = null;
+
+export default class Experience {
+    constructor (canvas) {
+        // singleton
+        if (instance) {
+            return instance
+        } 
+        instance = this
+
+        // setup
+        this.debug = new Debug()
+        this.canvas = canvas
+        this.sizes = new Sizes()
+        this.time = new Time()
+        this.scene = new THREE.Scene()
+        this.resources = new Resources(sources)
+        this.camera = new Camera()
+        this.renderer = new Renderer()
+        this.world = new World()
+        
+        this.sizes.on('resize', () => {
+            this.resize()
+        })
+
+        this.time.on('tick', () => {
+            this.update()
+        })
+
+    }
+
+    resize () {
+        this.camera.resize()
+        this.renderer.resize()
+
+    }
+
+    update () {
+        this.camera.update()
+        this.world.update()
+        this.renderer.update()
+    }
+
+    destroy () {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        // Traverse the whole scene
+        this.scene.traverse((child) => {
+            if(child instanceof THREE.Mesh) {
+                child.geometry.dispose()
+
+                for (const key in child.material) {
+                    const value = child.material[key]
+                    if( value && typeof value.dispose === 'function') {
+                        value.dispose()
+                    }
+
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+
+        if(this.debug.active){
+            this.debug.ui.destroy()
+        }
+    }
+} 
