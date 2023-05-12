b67eea7611f91a255a1370e330547fe9468e989b
nizar7zak <nizar7zakout@gmail.com>
2023-05-12 15:15:20 +0200

    useFrame

--- chapter7/42-first-r3f-application/src/Experince.jsx
+++ chapter7/42-first-r3f-application/src/Experince.jsx
@@ -1,27 +1,36 @@
-
-
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
 const Experience = () => {
+    const groupRef = useRef()
+
+    useFrame((state, delta) => {
+        groupRef.current.rotation.y += delta;        
+
+
+    })
   return (
-    <group>
-        <mesh
-            position-x ={2}
-        >
-            <boxGeometry/>
-            <meshBasicMaterial color="mediumpurple" />
-        </mesh>
+    <>
+        <group ref={groupRef}>
+            <mesh
+                position-x ={2}
+                >
+                <boxGeometry/>
+                <meshBasicMaterial color="mediumpurple" />
+            </mesh>
 
-        <mesh
-            position-x ={-2}
-        >
-            <sphereGeometry />
-            <meshBasicMaterial color="orange" />
-        </mesh>
+            <mesh
+                position-x ={-2}
+                >
+                <sphereGeometry />
+                <meshBasicMaterial color="orange" />
+            </mesh>
 
-        <mesh rotation-x={ - Math.PI * 0.5 } position-y= {-1}  scale={15} >
-            <planeGeometry  />
-            <meshBasicMaterial color="green" />
-        </mesh>
         </group>
+            <mesh rotation-x={ - Math.PI * 0.5 } position-y= {-1}  scale={15} >
+                <planeGeometry  />
+                <meshBasicMaterial color="green" />
+            </mesh>
+    </>
   )
 }
 
