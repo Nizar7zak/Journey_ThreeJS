30072a5bcb17f12aa7c0c6d25825a45d969855bd
nizar7zak <nizar7zakout@gmail.com>
2023-05-17 14:00:50 +0200

    import Clone

--- chapter7/46-load-models-with-r3f/src/Hamburger.jsx
+++ chapter7/46-load-models-with-r3f/src/Hamburger.jsx
@@ -1,12 +1,14 @@
-import { useGLTF } from "@react-three/drei"
+import {Clone, useGLTF } from "@react-three/drei"
 const Hamburger = () => {
 
     // const model = useGLTF('./hamburger.glb')
     const model = useGLTF('./hamburger-draco.glb')
-  return (
-    <primitive object={model.scene} scale={0.35} />
-   
-  )
+  return <>
+    <Clone object={model.scene} scale={0.35} position-x={5} />
+    <Clone object={model.scene} scale={0.35} />
+    <Clone object={model.scene} scale={0.35} position-x={-5} /> 
+  </>
+  
 }
 useGLTF.preload('./hamburger-draco.glb')
 export default Hamburger
