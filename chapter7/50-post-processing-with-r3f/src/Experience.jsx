6faf1c8408d8a3b69bfb6dac834d71e199b3a2a0
nizar7zak <nizar7zakout@gmail.com>
2023-05-22 15:40:51 +0200

    SSR

--- chapter7/50-post-processing-with-r3f/src/Experience.jsx
+++ chapter7/50-post-processing-with-r3f/src/Experience.jsx
@@ -1,12 +1,42 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { Vignette, EffectComposer, Glitch, Noise, Bloom, DepthOfField } from '@react-three/postprocessing'
+import { Vignette, EffectComposer, Glitch, Noise, Bloom, DepthOfField, SSR } from '@react-three/postprocessing'
 import { GlitchMode, BlendFunction } from 'postprocessing'
-
+import { useControls } from 'leva'
 
 // npm i @react-three/postprocessing@2.6
 export default function Experience()
 {
+    const ssrProps = useControls('SSR Effect', {
+        temporalResolve: true,
+        STRETCH_MISSED_RAYS: true,
+        USE_MRT: true,
+        USE_NORMALMAP: true,
+        USE_ROUGHNESSMAP: true,
+        ENABLE_JITTERING: true,
+        ENABLE_BLUR: true,
+        temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+        temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+        maxSamples: { value: 0, min: 0, max: 1 },
+        resolutionScale: { value: 1, min: 0, max: 1 },
+        blurMix: { value: 0.5, min: 0, max: 1 },
+        blurKernelSize: { value: 8, min: 0, max: 8 },
+        blurSharpness: { value: 0.5, min: 0, max: 1 },
+        rayStep: { value: 0.3, min: 0, max: 1 },
+        intensity: { value: 1, min: 0, max: 5 },
+        maxRoughness: { value: 0.1, min: 0, max: 1 },
+        jitter: { value: 0.7, min: 0, max: 5 },
+        jitterSpread: { value: 0.45, min: 0, max: 1 },
+        jitterRough: { value: 0.1, min: 0, max: 1 },
+        roughnessFadeOut: { value: 1, min: 0, max: 1 },
+        rayFadeOut: { value: 0, min: 0, max: 1 },
+        MAX_STEPS: { value: 20, min: 0, max: 20 },
+        NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+        maxDepthDifference: { value: 3, min: 0, max: 10 },
+        maxDepth: { value: 1, min: 0, max: 1 },
+        thickness: { value: 10, min: 0, max: 10 },
+        ior: { value: 1.45, min: 0, max: 2 }
+    })
     return <>
         <color args={['#ffffff']} attach="background" />
         <EffectComposer >
@@ -14,30 +44,32 @@ export default function Experience()
                 offset={0.3}
                 darkness={0.9}
                 blendFunction={BlendFunction.NORMAL}
-            /> */}
-            {/* <Glitch 
+            />
+            <Glitch 
                 delay={[0.5, 1]}
                 duration={[ 0.1, 0.3]}
                 strength={[0.2, 0.4]}
                 mode={GlitchMode.CONSTANT_WILD}
-            /> */}
-            {/* <Noise 
+            />
+            <Noise 
                 premultiply
                 blendFunction={BlendFunction.SOFT_LIGHT}
-            /> */}
-            {/* <Bloom 
+            />
+            <Bloom 
                 mipmapBlur
                 intensity={0.1}
                 luminanceThreshold={0}
 
-            /> */}
+            />
             <DepthOfField 
             focusDistance={ 0.025 }
             focalLength={ 0.025 }
             bokehScale= { 6 }
 
+            /> */}
+            <SSR 
+                {...ssrProps}
             />
-
         </EffectComposer>
         <Perf position="top-left" />
 
@@ -58,7 +90,7 @@ export default function Experience()
 
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+            <meshStandardMaterial color="greenyellow" metalness={0} roughness={0} />
         </mesh>
 
     </>
