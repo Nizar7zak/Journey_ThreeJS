e9eb6a0e5717de310cab8cee02988cd9b0fc823b
nizar7zak <nizar7zakout@gmail.com>
2023-05-22 09:31:18 +0200

    performance solution

--- chapter7/47-3d-text-with-r3f/src/Experience.jsx
+++ chapter7/47-3d-text-with-r3f/src/Experience.jsx
@@ -1,35 +1,29 @@
 import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useState } from 'react'
 
 
 export default function Experience()
 {   
+
+    const [ torusGeometry, setTorusGeometry ] = useState()
+    const [ material, setMaterial ] = useState()
+
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
-        <Center>
-            <Text3D 
-                font={'./fonts/helvetiker_regular.typeface.json'}
-                size={0.75}
-                height={0.2}
-                curveSegments={12}
-                bevelEnabled
-                bevelThickness={0.02}
-                bevelSize={0.02}
-                bevelSegments={5}
-                bevelOffset={0}
-            >
-                Hello Rf3
-                <meshMatcapMaterial matcap={matcapTexture} />
-            </Text3D>
-        </Center>
 
+        <torusGeometry ref={setTorusGeometry} args={[ 1, 0.6, 16, 32 ]} />                
+        <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
         {
             [...Array(100)].map((item, index) => {
-                return <mesh key={index}
+                return <mesh 
+                    geometry={torusGeometry}
+                    material={material}
+                    key={index}
                     position={[
                         (Math.random() - 0.5 ) * 10,
                         (Math.random() - 0.5 ) * 10,
@@ -44,11 +38,27 @@ export default function Experience()
                         0
                     ]}
                 >
-                <torusGeometry args={[ 1, 0.6, 16, 32 ]} />
-                <meshMatcapMaterial matcap={matcapTexture} />
             </mesh>
             })
         }
+
+        <Center>
+            <Text3D 
+                material={material}
+                font={'./fonts/helvetiker_regular.typeface.json'}
+                size={0.75}
+                height={0.2}
+                curveSegments={12}
+                bevelEnabled
+                bevelThickness={0.02}
+                bevelSize={0.02}
+                bevelSegments={5}
+                bevelOffset={0}
+            >
+                Hello Rf3
+            </Text3D>
+        </Center>
+
         
      
 
