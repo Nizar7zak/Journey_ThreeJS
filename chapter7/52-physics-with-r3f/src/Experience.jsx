2420f59ed8a76d6e0f111bf0cbef485355fb3e0b
nizar7zak <nizar7zakout@gmail.com>
2023-05-23 15:54:23 +0200

    ball colliders

--- chapter7/52-physics-with-r3f/src/Experience.jsx
+++ chapter7/52-physics-with-r3f/src/Experience.jsx
@@ -14,17 +14,19 @@ export default function Experience()
 
         <Physics>
             <Debug />
-            <RigidBody>
+            <RigidBody colliders='ball'>
                 <mesh castShadow position={ [ - 2, 2, 0 ] }>
                     <sphereGeometry />
                     <meshStandardMaterial color="orange" />
                 </mesh>
             </RigidBody>
 
-            <mesh castShadow position={ [ 2, 2, 0 ] }>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
+            <RigidBody>
+                <mesh castShadow position={ [ 2, 2, 0 ] }>
+                    <boxGeometry />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </RigidBody>
 
             <RigidBody type='fixed'>
                 <mesh receiveShadow position-y={ - 1.25 }>
