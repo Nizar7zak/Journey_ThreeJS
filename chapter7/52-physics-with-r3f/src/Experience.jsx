55790f91f8d647c5db4b448c731d95cdcf434ed8
nizar7zak <nizar7zakout@gmail.com>
2023-05-24 10:38:26 +0200

    apply forces

--- chapter7/52-physics-with-r3f/src/Experience.jsx
+++ chapter7/52-physics-with-r3f/src/Experience.jsx
@@ -1,10 +1,21 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { CuboidCollider, Debug, RigidBody, Physics } from '@react-three/rapier'
+import { useRef } from 'react'
 export default function Experience()
-{
-    return <>
+{   
+    const cube = useRef()
 
+    const handleJump = () => {
+        cube.current.applyImpulse({ x: 0, y: 5, z: 0})
+        cube.current.applyTorqueImpulse({ 
+            x: Math.random() - 0.5,
+            y: Math.random() - 0.5,
+            z: Math.random() - 0.5
+        })
+    }
+    return <>
+        
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
@@ -15,13 +26,21 @@ export default function Experience()
         <Physics>
             <Debug />
             <RigidBody colliders='ball'>
-                <mesh castShadow position={ [ 0, 4, 0 ] }>
+                <mesh castShadow position={ [ -1.5, 2, 0 ] }>
                     <sphereGeometry />
                     <meshStandardMaterial color="orange" />
                 </mesh>
             </RigidBody>
 
-            <RigidBody colliders={false} rotation-x={Math.PI * 0.5} position={ [ 0, 1, 0 ] }>
+            <RigidBody ref={cube}>
+                <mesh castShadow position={ [ 1.5, 2, 0 ] } onClick={handleJump} >
+                    <boxGeometry />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </RigidBody>
+
+
+            {/* <RigidBody colliders={false} rotation-x={Math.PI * 0.5} position={ [ 0, 1, 0 ] }>
                 <CuboidCollider args={[1.5, 1.5, 0.5]} />
                 <CuboidCollider args={[1, 1, 1]} position={[ 0, 0, 1]} rotation={[ -Math.PI *0.35, 0, 0 ]} />
                 <mesh 
@@ -32,8 +51,7 @@ export default function Experience()
                     />
                     <meshStandardMaterial color={'mediumpurple'} /> 
                 </mesh>
-            </RigidBody>
-
+            </RigidBody> */}
 
             <RigidBody type='fixed'>
                 <mesh receiveShadow position-y={ - 1.25 }>
